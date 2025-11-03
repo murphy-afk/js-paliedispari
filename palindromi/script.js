e597e3611a0f24c4fb79d8429dac4ca19dc0cacb
murphy-afk <bardarenoemi@gmail.com>
2025-11-03 15:44:00 +0100

    feat: refactoring

--- palindromi/script.js
+++ palindromi/script.js
@@ -5,29 +5,26 @@
 // posso fare due for che scorrono contemporaneamente in direzioni opposte e controllano
 // che le lettere siano uguali  => NOPE
 
-const word = "anna";
-console.log(word);
-
+function isPalindrome(word) {
 let length = word.length;
-// for (let i = 0; i < length; i++) {
-//   const left = word[i];
-//   console.log(left);
-// }
-// for (let j = length; j >= 0; j--) {
-//     const right = word[j];
-//     console.log(right);
-//   }
-let isPalindrome = false;
+let result = false;
 for (let i = 0; i <= length; i++) {
 if (word[i] === word[length]) {
-  isPalindrome = true;
+  result = true;
 }
   length--;
 }
-if (isPalindrome === true) {
-  console.log("è palindroma");
+return result
+}
+
+const userWord = prompt("Inserisci una parola è ti dirò se è palindroma");
+console.log(userWord);
+const answer = isPalindrome(userWord);
+if (answer === true) {
+  console.log("La parola è palindroma");
 }
 else {
-  console.log("non è palindroma");
+  console.log("La parola non è palindroma");
 }
+
 
