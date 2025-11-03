4467f5b5ca292de02a6836f213b7455470add7d4
murphy-afk <bardarenoemi@gmail.com>
2025-11-03 15:02:49 +0100

    feat: odd or even function + winner output

--- pari-e-dispari/script.js
+++ pari-e-dispari/script.js
@@ -5,13 +5,44 @@
 // Dichiariamo chi ha vinto.
 
 // param: numero massimo 
+// return: numero
 function genRandom(n) {
   const randomNumber = Math.floor(Math.random() * n) + 1;
   return randomNumber;
 }
+// param: numero 
+// return: boleano
+// function isEven(n) {
+//   const even = n % 2 === 0;
+//   return even
+// }
 
+// param: numero
+// return: string (pari o dispari)
+function isOddOrEven(n) {
+  let result;
+  if (n % 2 === 0) {
+    result = "pari";
+  }
+  else {
+    result = "dispari";
+  }
+  return result
+}
 
 const oddOrEven = prompt("scegli pari o dispari");
-const userNumber = prompt("scegli un numero tra 1 e 5")
+const userNumber = prompt("scegli un numero tra 1 e 5");
 // poi aggiungere conrtrolli
 const comuputerNumber = genRandom(5);
+const sum = parseInt(userNumber) + comuputerNumber;
+console.log(oddOrEven, userNumber, comuputerNumber, sum);
+const result = isOddOrEven(sum);
+let winner;
+if (result === oddOrEven) {
+  winner = "user";
+}
+else {
+  winner = "computer";
+}
+console.log(`il vincitore è ${winner}`);
+
