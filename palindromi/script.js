ea3fb2d1cc023643856d0d3a5f3b0a983f7841ec
murphy-afk <bardarenoemi@gmail.com>
2025-11-03 16:07:06 +0100

    fix: logical error

--- palindromi/script.js
+++ palindromi/script.js
@@ -2,19 +2,18 @@
 // Creare una funzione per capire se la parola inserita è palindroma 
 // (NON usare split, reverse e join)
 
-// posso fare due for che scorrono contemporaneamente in direzioni opposte e controllano
-// che le lettere siano uguali  => NOPE
-
 function isPalindrome(word) {
-let length = word.length;
-let result = false;
-for (let i = 0; i <= length; i++) {
-if (word[i] === word[length]) {
-  result = true;
-}
-  length--;
-}
-return result
+  let length = word.length;
+  let result = false;
+  let j = length - 1;
+  for (let i = 0; i < length; i++) {
+    if (word[i] === word[j]) {
+      result = true;
+    }
+    console.log(word[i], word[j], result);
+    j = j - 1;
+  }
+  return result
 }
 
 const userWord = prompt("Inserisci una parola è ti dirò se è palindroma");
